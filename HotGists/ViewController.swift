5e706a5acee055f834c4b79581d1bf8a17016507
Benjamin, Zhong Binghui <zhongbinghui1995@gmail.com>
2022-02-26 22:55:47 +0800

    Initial Commit

---
+++ HotGists/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HotGists
+//
+//  Created by Binghui Zhong on 26/2/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

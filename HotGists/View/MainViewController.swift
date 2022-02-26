9e6984bdeecb95c25496d7076f1f93f06d2f1773
Benjamin, Zhong Binghui <zhongbinghui1995@gmail.com>
2022-02-26 23:09:04 +0800

    - first setup project - remove storyboard

---
+++ HotGists/View/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  HotGists
+//
+//  Created by Binghui Zhong on 26/2/2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        self.view.backgroundColor = .red
+    }
+    
+}
